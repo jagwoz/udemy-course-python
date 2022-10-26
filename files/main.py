799495692998945374c0ba4d1f438c3b70d68e59
jagwoz <jagwoz.kontakt@gmail.com>
2022-10-26 22:58:14 +0200

    Commit 26.10.2022

---
+++ files/main.py
@@ -0,0 +1,20 @@
+if __name__ == "__main__":
+    file = open('file', 'w')
+    try:
+        # r - read, w - write, a - append
+        # r+ - read and write, w+ - write and read, a+ - append (write only to end) and read
+        file.write('text')
+    finally:
+        file.close()
+
+    with open('file', 'a') as file:
+        file.write('text')
+
+    with open('file', 'r', encoding='UTF-8') as file:
+        first_line = file.readline()
+        next_line = file.readline()
+        all_with_slash_ns = file.readlines()
+        text_list = file.read().splitlines()
+        # for line in file:
+        actual_position = file.tell()
+        file.seek(0)  # jump to start position
---
+++ generatic/main.py
@@ -0,0 +1,22 @@
+import random
+
+
+def my_generic_function():
+    nr = 0
+    while True:
+        nr = yield nr + random.randint(0, 50)
+
+
+generator = my_generic_function()
+
+# for i in range(10):
+#    print(next(generator))
+
+# print(list(generator))  # all next
+
+next(generator)
+# generator.send(None)
+
+print("=")
+print(generator.send(500))
+
